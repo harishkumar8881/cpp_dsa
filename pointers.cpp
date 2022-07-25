de458dcf57c42d8170aca3dd5f8232e5edac93eb
Harish Kumar <harishkumar8881@gmail.com>
2022-07-25 21:34:20 +0530

    updated pointers

--- pointers.cpp
+++ pointers.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+int addByValue(int a, int b){
+    return a + b;
+}
+
+int addByReference(int& a, int& b){
+    return a + b;
+}
+
+int addByReference2(int *a, int *b){
+    return *a + *b;
+}
+
 int main()
 {
     int a = 12, b = 13;
@@ -10,5 +22,8 @@ int main()
 
     cout << "value at p " << *p << endl;
     cout << "address of p " << p << endl;
+    cout << "add a and b by value " << addByValue(a, b) << endl;
+    cout << "add a and b by reference " << addByReference(a, b) << endl;
+    cout << "add a and b by reference2 " << addByReference2(&a, &b) << endl;
     return 0;
 }
